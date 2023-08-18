70bdc9ccd96c3f06b6b43e3ec3cf80fd52c07702
Silvio Traversaro <silvio@traversaro.it>
2023-08-18 11:14:56 +0200

    Enable CUDA support

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,25 @@
 export OMP_CANCELLATION=TRUE
 export OMP_PROC_BIND=TRUE
 
+if [[ ! -z "${cuda_compiler_version+x}" && "${cuda_compiler_version}" == "11.*" ]]
+    if [[ -z "${CUDA_HOME+x}" ]]
+    then
+        echo "==> cuda_compiler_version=${cuda_compiler_version}, extract manually CUDA_HOME variable"
+        CUDA_GDB_EXECUTABLE=$(which cuda-gdb || exit 0)
+        if [[ -n "$CUDA_GDB_EXECUTABLE" ]]
+        then
+            CUDA_HOME=$(dirname $(dirname $CUDA_GDB_EXECUTABLE))
+        else
+            echo "Cannot determine CUDA_HOME: cuda-gdb not in PATH"
+            return 1
+        fi
+    fi
+    export NVCCFLAGS="${NVCCFLAGS} -shared -Xcompiler -fPIC"
+    export CUDAOPTIONS="--enable-gpu"
+then
+    export CUDAOPTIONS="--disable-gpu"
+fi
+
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib -lcblas"
 export CFLAGS="$CFLAGS -fPIC"
 export CPPFLAGS="$CPPFLAGS -fPIC"
@@ -12,7 +31,7 @@ export FCFLAGS="$FCFLAGS -fPIC"
 ./autogen.sh
 mkdir build
 cd build
-../configure --with-metis="-lmetis" --prefix=$PREFIX
+../configure $CUDAOPTIONS --with-metis="-lmetis" --prefix=$PREFIX
 make
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
