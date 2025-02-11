a35166314158e521094c03e419d3f364764f3e18
Silvio Traversaro <silvio@traversaro.it>
2025-02-11 09:14:03 +0100

    Use crosscompiling_emulator for running meson test in cross-compilation

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,17 @@ export LDFLAGS="${LDFLAGS} -lcblas"
 
 meson setup builddir ${MESON_ARGS} -Dexamples=false -Dtests=true -Dmodules=false -Dgpu=false
 
+if [[  "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" && "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+  # Create a custom crosscompiling_emulator_meson_cross_file.txt to specify the emulator to use for tests
+  # and avoid that meson test passes by skipping the tests, and add it to MESON_FLAGS to combine it with
+  # the meson_cross_file.txt generated in
+  # https://github.com/conda-forge/ctng-compiler-activation-feedstock/blob/main/recipe/activate-gcc.sh
+  # https://github.com/conda-forge/clang-compiler-activation-feedstock/blob/main/recipe/activate-clang.sh
+  echo "[binaries]" >> "$(pwd)/crosscompiling_emulator_meson_cross_file.txt"
+  echo "exe_wrapper = '${CROSSCOMPILING_EMULATOR}'" >> "$(pwd)/crosscompiling_emulator_meson_cross_file.txt"
+  export MESON_ARGS="${MESON_ARGS} --cross-file $(pwd)/crosscompiling_emulator_meson_cross_file.txt"
+fi
+
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
   # Required for tests to pass, see https://github.com/ralna/spral#usage-at-a-glance
   # and https://github.com/ralna/spral/issues/7#issuecomment-288700497
