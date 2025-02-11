7de4f4d64c770c7e42bc2d5741de022f955f4a9f
Silvio Traversaro <silvio@traversaro.it>
2025-02-11 13:30:31 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ export PKG_CONFIG=$BUILD_PREFIX/bin/pkg-config
 
 export LDFLAGS="${LDFLAGS} -lcblas"
 
+export MESON_TEST_EXTRA_ARGS=""
 if [[  "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" && "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
   # Create a custom crosscompiling_emulator_meson_cross_file.txt to specify the emulator to use for tests
   # and avoid that meson test passes by skipping the tests, and add it to MESON_FLAGS to combine it with
@@ -15,7 +16,7 @@ if [[  "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" && "${CROSSCOMPILING_EMULATOR
   echo "exe_wrapper = '${CROSSCOMPILING_EMULATOR}'" >> "$(pwd)/crosscompiling_emulator_meson_cross_file.txt"
   export MESON_ARGS="${MESON_ARGS} --cross-file $(pwd)/crosscompiling_emulator_meson_cross_file.txt"
   # Tests last longer in emulation, so let's increase the timeouts
-  export MESON_ARGS="${MESON_ARGS} --timeout-multiplier 2"
+  export MESON_TEST_EXTRA_ARGS="${MESON_TEST_EXTRA_ARGS} --timeout-multiplier 2"
 fi
 
 meson setup builddir ${MESON_ARGS} -Dexamples=false -Dtests=true -Dmodules=false -Dgpu=false
@@ -25,7 +26,7 @@ if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}
   # and https://github.com/ralna/spral/issues/7#issuecomment-288700497
   export OMP_CANCELLATION=TRUE
   export OMP_PROC_BIND=TRUE
-  meson test -C builddir
+  meson test -C builddir ${MESON_TEST_EXTRA_ARGS}
 fi
 
 # Disable tests before install to avoid tests being installed
